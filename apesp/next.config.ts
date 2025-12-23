8e3b0dcb069c4f26a2cbee5963d71ed918e70b7c
atiujjwal <ujjwalvinamridu@gmail.com>
2025-12-24 00:44:21 +0530

    add security headers

--- apesp/next.config.ts
+++ apesp/next.config.ts
@@ -10,6 +10,42 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  // --- Security Headers ---
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
+          },
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; img-src 'self' blob: data: https:; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline';",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
